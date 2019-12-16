5afda87e678a3de77a4f6c446f3d6a887e7ead40
Benedikt Groß <bg@benedikt-gross.de>
2019-12-16 13:22:47 -0500

    Added npm postinstall hook to automate openmoji /data folder updates

--- gulpfile.js
+++ gulpfile.js
@@ -18,3 +18,28 @@ gulp.task('sass', function () {
       errorHandler: onError
     }));
 });
+
+gulp.task('copy-openmoji-data-json', function(){
+  return gulp.src([
+    'node_modules/openmoji/package.json',
+    'node_modules/openmoji/data/color-palette.json',
+    'node_modules/openmoji/data/openmoji.json',
+    ]).pipe(gulp.dest('data/'));
+});
+
+gulp.task('copy-openmoji-data-black-svg', function(){
+  return gulp.src('node_modules/openmoji/black/svg/*.svg')
+    .pipe(gulp.dest('data/black/svg/'));
+});
+
+gulp.task('copy-openmoji-data-color-svg', function(){
+  return gulp.src('node_modules/openmoji/color/svg/*.svg')
+    .pipe(gulp.dest('data/color/svg/'));
+});
+
+gulp.task('copy-openmoji-data',
+  gulp.series(
+    'copy-openmoji-data-json',
+    'copy-openmoji-data-black-svg',
+    'copy-openmoji-data-color-svg'
+));
