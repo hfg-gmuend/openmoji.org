2fd8e1a3eaeaa201a291f3d460c6522e9f5880e2
Stephan Bogner <hello@stephanbogner.de>
2021-12-14 12:51:37 +0100

    Fixed color picker (also with url adjusting and loading)

--- public/js-code/skintone-colorpicker.js
+++ public/js-code/skintone-colorpicker.js
@@ -3,13 +3,32 @@ let multiColors = {
 	'right': null
 }
 
+pickCorrectVariantAccordingToUrl();
+
 function colorChangeSingleColor(skintoneId){	
-	const resultingEmojiHex = SKINTONECOMBINATIONS[skintoneId];
-	adjustView(resultingEmojiHex);
+	$('.circle').removeClass('highlight');
+
+	if(skintoneId === 'black'){
+		$('.emoji-variant-color').addClass('hidden')
+		$('.emoji-variant-black').removeClass('hidden')
+		$('#outline-emoji-preview').addClass('highlight')
+		adjustViewAndUrl('black', false);
+	}else if(skintoneId === ''){
+		$('.emoji-variant-color').removeClass('hidden')
+		$('.emoji-variant-black').addClass('hidden')
+		$('#color-emoji-preview').addClass('highlight')
+		const resultingEmojiHex = SKINTONECOMBINATIONS[skintoneId];
+		adjustViewAndUrl(skintoneId, resultingEmojiHex);
+	}else{
+		$('.emoji-variant-color').removeClass('hidden')
+		$('.emoji-variant-black').addClass('hidden')
+		$('.skintone-selector > .circle:eq(' + (Number(skintoneId)-1) + ')').addClass('highlight');
+		const resultingEmojiHex = SKINTONECOMBINATIONS[skintoneId];
+		adjustViewAndUrl(skintoneId, resultingEmojiHex);
+	}
 }
 
 function colorChangeMultiColor(side, skintoneId){
-	console.log(side, skintoneId);
 	multiColors[side] = skintoneId;
 
 	if(!multiColors.left){
@@ -24,8 +43,36 @@ function colorChangeMultiColor(side, skintoneId){
 	if(multiColors.left === multiColors.right){
 		skintoneCode = multiColors.left;
 	}
+
+	$('.emoji-variant-color').removeClass('hidden')
+	$('.emoji-variant-black').addClass('hidden')
+	$('.skintone-selector > .circle').removeClass('highlight');
+	$('.single-color').removeClass('highlight');
+	$('.skintone-selector.left > .circle:eq(' + (multiColors.left-1) + ')').addClass('highlight');
+	$('.skintone-selector.right > .circle:eq(' + (multiColors.right-1) + ')').addClass('highlight');
+
 	const resultingEmojiHex = SKINTONECOMBINATIONS[skintoneCode];
-	adjustView(resultingEmojiHex);
+	adjustViewAndUrl(skintoneCode, resultingEmojiHex);
+}
+
+function adjustViewAndUrl(skintoneId, emojiHex){
+	if(skintoneId === ''){
+		exposeListFilter({variant: undefined})
+	}else if(skintoneId === 'black'){
+		exposeListFilter({variant: 'black'})
+	}else{
+		exposeListFilter({variant: emojiHex})
+	}
+
+	if(emojiHex){
+		const filePathSVG = getFilePathEmojiImage(emojiHex, 'svg');
+		const filePathPNG = getFilePathEmojiImage(emojiHex, 'png');
+		const unicodeCombinationLink = getEmojiCombinationLink(emojiHex);
+		$('#main-emoji-image-color').attr('src', filePathSVG);
+		$('#downloadSVG').attr('href', filePathSVG);
+		$('#downloadPNG').attr('href', filePathPNG);
+		$('#unicodeCombination').html(unicodeCombinationLink)
+	}
 }
 
 function getFilePathEmojiImage (hexCode, format = 'svg', blackOrColor = 'color'){
@@ -46,12 +93,90 @@ function getEmojiCombinationLink(hexcodeString){
   }).join(" • ");
 }
 
-function adjustView(emojiHex){
-	const filePathSVG = getFilePathEmojiImage(emojiHex, 'svg');
-	const filePathPNG = getFilePathEmojiImage(emojiHex, 'png');
-	const unicodeCombinationLink = getEmojiCombinationLink(emojiHex);
-	$('#main-emoji-image').attr('src', filePathSVG);
-	$('#downloadSVG').attr('href', filePathSVG);
-	$('#downloadPNG').attr('href', filePathPNG);
-	$('#unicodeCombination').html(unicodeCombinationLink)
+function getUrlParameters() {
+    var sPageURL = decodeURIComponent(window.location.hash.substring(1)),
+      sURLVariables = sPageURL.split("&"),
+      sParameters = {},
+      i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+      var currentParam = sURLVariables[i].split("=");
+      sParameters[currentParam[0]] = currentParam[1];
+    }
+    return sPageURL ? sParameters : undefined;
+}
+
+// function getUrlParameters() {
+// 	    var sPageURL = decodeURIComponent(window.location.hash.substring(1)),
+// 	      sURLVariables = sPageURL.split("&"),
+// 	      sParameters = {},
+// 	      i;
+
+// 	    for (i = 0; i < sURLVariables.length; i++) {
+// 	      var currentParam = sURLVariables[i].split("=");
+// 	      sParameters[currentParam[0]] = currentParam[1];
+// 	    }
+// 	    return sParameters;
+
+// 	    // If variant is present in the URL and the variant actually exists
+// 	    if(sParameters.variant && SKINTONESFORHEX[sParameters.variant]){
+// 	    	const hexCode = SKINTONESFORHEX[sParameters.variant];
+// 	    	const emojiHex = sParameters.variant;
+// 	    	const skintoneId = SKINTONESFORHEX[sParameters.variant];
+// 	    	//adjustViewAndUrl(skintoneId, emojiHex);
+// 	    	//updatePicker(emojiHex);
+// 	    }
+// }
+
+function exposeListFilter(filter) {
+    // add filter that already exist in URL to current filter, so they don't get lost
+    var currentUrlParams = getUrlParameters();
+    // if currentUrlParams are undefined create new object
+    if (currentUrlParams === undefined) currentUrlParams = {};
+    // remove undefined filters and add new filter to currentUrlParams or update existing ones
+    for (var key in filter) {
+      if (filter.hasOwnProperty(key)) {
+        if (filter[key] === undefined) {
+          // also delete in currentUrlParams if it exists there
+          if (currentUrlParams.hasOwnProperty(key)) {
+            delete currentUrlParams[key];
+          }
+          delete filter[key];
+        } else {
+          currentUrlParams[key] = filter[key];
+        }
+      }
+    }
+
+    // expose filter in URL
+    var URL;
+    if ($.isEmptyObject(currentUrlParams)) {
+      URL = window.location.href.substring(0, window.location.href.indexOf("#"));
+    } else if (window.location.href.includes("#")) {
+      URL = window.location.href.substring(0, window.location.href.indexOf("#") + 1);
+    } else {
+      URL = window.location.href.substring(0, window.location.href.indexOf("#")) + "#";
+    }
+    history.pushState(null, "", URL + $.param(currentUrlParams));
+}
+
+function updatePicker(skintoneId){
+	if(skintoneId.split(',').length > 1){
+		const skintoneIds = skintoneId.split(',');
+		colorChangeMultiColor('left', skintoneIds[0]);
+		colorChangeMultiColor('right', skintoneIds[1]);
+	}else{
+		colorChangeSingleColor(skintoneId)
+	}
+}
+
+function pickCorrectVariantAccordingToUrl(){
+	let urlParameters = getUrlParameters();
+	if(urlParameters.variant){
+		let skintoneId = urlParameters.variant
+		if(skintoneId !== 'black'){
+			skintoneId = SKINTONESFORHEX[urlParameters.variant];
+		}
+		updatePicker(skintoneId);
+	}
 }
