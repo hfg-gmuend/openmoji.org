202bbedcb2dfb56414696f64c228a86af4476873
s-light <git@s-light.eu>
2024-02-21 13:02:51 +0000

    fix massvie file changes (local build changes docs folder - is not allowed to commit normaly... :-( )

--- docs/php/download_asset.php
+++ docs/php/download_asset.php
@@ -19,7 +19,7 @@ switch ($asset_type) {
     $file_url  = "https://github.com/hfg-gmuend/openmoji/raw/master/" . $emoji_variant . "/618x618/" . $emoji_hexcode . ".png";
 
     // set headers
-    header("Content-Type: image/png");
+    header("Content-Type: application/octet-stream");
     header("Content-Transfer-Encoding: Binary");
     header("Content-disposition: attachment; filename=\"" . $emoji_hexcode . "_" . $emoji_variant . ".png\"");
 
